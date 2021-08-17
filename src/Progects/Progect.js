1299a99daa2263c0f40d0e94eb8dc6c16792af1e
lismgmk <lismgmk2@gmail.com>
2021-08-18 00:11:47 +0300

    lesson 2p10

--- src/Progects/Progect.js
+++ src/Progects/Progect.js
@@ -3,17 +3,31 @@ import {Button} from "../Components/Buttoon/Button";
 
 
 export function Progect(props) {
+
+
+    const progectFunc = props.func.map(i => {
+            return <div className={style.progectFuncItem}>{i}</div>
+        }
+    )
+    console.log(progectFunc)
     return (
         <div className={style.progectBox}>
             <div className={style.progectBoxIcon} style={props.style}>
-                <Button href={props.href} buttonName={'See project'}/>
+                <Button href={props.href2project} buttonName={props.buttonNameProject}/>
+                <Button href={props.href2code} buttonName={props.buttonNameCode}/>
             </div>
             <div className={style.progectBoxText}>
                 <h3 className={style.progectH3title}>{props.name}</h3>
-                <p>{props.text}</p>
+                <span className={style.progectSpan}>Used libraries: </span>
+                <p className={style.progecLibraris}>  {props.usedLibraries}</p>
+                <span className={style.progectSpan}>What can you do in this project: </span>
+                <p className={style.progectFunc}> {Array.isArray(props.func) && progectFunc}</p>
+
+                <p className={style.psNotification}>* If the project cannot be loaded, it means it is in the "loading"
+                    state to GitHub Pages. In this case see the code. This project is in development status... </p>
             </div>
         </div>
-    );
+    )
 }
 
 
--- src/Progects/Progects.js
+++ src/Progects/Progects.js
@@ -4,37 +4,107 @@ import {Progect} from "./Progect";
 import {TitleH2} from "../Components/Title/TitleH2";
 import socialImage from "../assets/images/socialNetwork.jpg";
 import toDoListImage from "../assets/images/todoList.jpg";
+import customItem from "../assets/images/customItems.jpg";
+import pokraskino from "../assets/images/pokraskino.jpg";
 import * as React from "react";
+import slide1 from "../assets/images/slide1me.jpg";
+import slide2 from "../assets/images/slide2progects.jpg";
+import slide3 from "../assets/images/slide3skils.png";
 
 
-export const Progects = (props)=> {
-    const socialImageStyle = {
-        backgroundImage: `url(${socialImage})`
-    };
-    const toDoListImageStyle = {
-        backgroundImage: `url(${toDoListImage})`
-    };
+export const Progects = (props) => {
+
+    const funcActions = {
+        SocialNetwork: [
+            'Log in and log out',
+            'Download from server and see your profile',
+            'Change the status of your profile',
+            'Write a private message',
+            'Download from the server and see the profiles of other users',
+            'Subscribing and unsubscribing to a user'
+        ],
+        toDoList: [
+            'creating new todolists with nested tasks',
+            'create-read-update-delete operations with tudulists and tasks, sending to the server',
+            'login logout, handling errors sending requests',
+        ],
+        customComponents: [
+            'creation of custom inputs, selectors, buttons',
+            'sorting the list',
+            'work with object Date and other',
+        ],
+        pokraskino: [
+            'A valid selling landing page. Created on the WordPress constructor using Elementor. Configured targeting advertising in Google and Yandex accounts. Not a bad CEO. Promotion in social networks: VK (1500 subscribers), Instagram (812 subscribers)',
+        ],
+
+    }
+
+
+    const projectsInfo = [
+        {
+            style: {backgroundImage: `url(${socialImage})`},
+            name: "Social Network",
+            href2project: 'https://lismgmk.github.io/Social-Network/',
+            href2code: 'https://github.com/lismgmk/Social-Network',
+            usedLibraries: 'REACT, REDUX, TYPESCRIPT, AXIOS, MATERIAL UI, REDUX-FORM, react-router, redux-thunk, reselect',
+            func: funcActions.SocialNetwork,
+            buttonNameProject: 'see progect*',
+            buttonNameCode: 'see code',
+        },
+        {
+            style: {backgroundImage: `url(${toDoListImage})`},
+            name: "Todo list",
+            href2project: 'https://lismgmk.github.io/ToDoList',
+            href2code: 'https://github.com/lismgmk/ToDoList',
+            usedLibraries: 'REACT, Redux Toolkit, TYPESCRIPT, AXIOS, MATERIAL UI, formik, react-router, redux-thunk, immer, uuid',
+            func: funcActions.toDoList,
+            buttonNameProject: 'see progect*',
+            buttonNameCode: 'see code',
+        },
+        {
+            style: {backgroundImage: `url(${customItem})`},
+            name: "Custom elements",
+            href2project: 'https://lismgmk.github.io/ignat/',
+            href2code: 'https://github.com/lismgmk/ignat',
+            usedLibraries: 'REACT, Redux, TYPESCRIPT, AXIOS, MATERIAL UI, react-router',
+            func: funcActions.customComponents,
+            buttonNameProject: 'see progect*',
+            buttonNameCode: 'see code',
+        },{
+            style: {backgroundImage: `url(${pokraskino})`},
+            name: "Landing page for business",
+            href2project: 'https://pokraskino.by/',
+            href2code: 'https://pokraskino.by/',
+            usedLibraries: 'WORDPRESS, HTML, CSS, GOOGLE ADS, YANDEX DIRECT, VK, INSTAGRAM',
+            func: funcActions.pokraskino,
+            buttonNameProject: 'see progect*',
+            buttonNameCode: 'see code',
+        },
+
+
+    ]
+
 
     return (
         <div id='progects' className={elseStyle.rawBox}>
             <div className={`${style.progects} ${elseStyle.container}`}>
                 <TitleH2 text={'My projects'}/>
-                <div className={style.progectsBlock}>
-                    <Progect
-                        href={'https://lismgmk.github.io/Social-Network/'}
-                        style={socialImageStyle}
-                        name={'Social Network'}
-                        text={'My social network is an opportunity to communicate with friends, listen to music, exchange photos and likes, as well as make new acquaintances.'}
-                    />
-                    <Progect
-                        href={'https://lismgmk.github.io/ToDoList/'}
-                        style={toDoListImageStyle}
-                        img={'image2'}
-                        name={'ToDo List'}
-                        text={'My todo list will help you not to forget about your business'}
-                    />
-
-                </div>
+                <>
+                    <div className={style.progectsBlock}>
+                        {projectsInfo.map(i => {
+                            return <Progect
+                                style={i.style}
+                                name={i.name}
+                                href2project={i.href2project}
+                                href2code={i.href2code}
+                                usedLibraries={i.usedLibraries}
+                                func={i.func}
+                                buttonNameProject={i.buttonNameProject}
+                                buttonNameCode={i.buttonNameCode}
+                            />
+                        })}
+                    </div>
+                </>
             </div>
         </div>
     )
